d6b20df20156ae82bb381c17c9b120df85ce7587
Federico <94gfede@gmail.com>
2018-11-30 17:57:42 +0100

    carico script massimi

---
+++ prova_massimi.m
@@ -0,0 +1,19 @@
+
+% Ora richiedo maggiore precisione all'integratore (interpolazione polinomiale a 32 punti)
+%vedere cosa è x0
+
+x0 = [0 0.1 0];
+opt = odeset('RelTol', 1.0e-5, ...
+             'Events', @HRxmaxy, ...
+             'Refine', 2);
+[T, X, Te, Xe, Ie] = ode45(@osc, [0 ,100], x0, opt);
+% grafici
+figure;
+plot(T,X(:,1),'b');
+hold on;
+%plot(Te,X(:,1),'r');
+% Questa volta con la procedura grezza va meglio
+[p, t] = dsfindpeaks(X(:, 1));
+plot(T(p),X(p,1),'ko');
+plot(Te,X(p,1),'r');
+
